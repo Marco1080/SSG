9689155f1dca1344980b7588af536a4f526aaa61
Marco <93841465+Marco1080@users.noreply.github.com>
2024-11-29 13:01:11 +0000

    Act 7

---
+++ UT2 Instalación y configuración de sistemas ERP-CRM/Python_7_MALE.py
@@ -0,0 +1,15 @@
+# ASGMT dicts
+
+# asigna valores iniciales
+words = 'aro|clara|patata|brillo'
+letter = 'a'
+palabras = words.split('|')
+letter_counter = {}
+
+
+for palabra in palabras:
+    contador = palabra.count(letter)
+
+    letter_counter[palabra] = contador
+    
+print(letter_counter)
